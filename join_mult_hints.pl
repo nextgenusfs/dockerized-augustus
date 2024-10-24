338df56d3e56e8217daba2ff44526627b31a38b7
Jonathan Palmer <jon@Palmers-MB.local>
2024-10-23 22:02:57 -0700

    make this one just the perl script as docker stdin is an issue

--- join_mult_hints.pl
+++ join_mult_hints.pl
@@ -1,41 +1,100 @@
-#!/usr/bin/env bash
+#!/usr/bin/env perl
+#
+# summarize multiple identical hints to one with mult=n
+#
+# This script is used by the braker.pl pipeline.
+# Please be extremely careful when changing this script because the braker.pl
+# pipeline may fail upon custom modification of this script.
+# In case of doubt, contact katharina.hoff@uni-greifswald.de
+#
+# Mario Stanke, 4.1.2010, last modification by Katharina J. Hoff on March 1st 2018
 
-# this is a hacky way to run dockerized linux application locally
+use strict;
+use Getopt::Long;
 
-realpath() {
-  OURPWD=$PWD
-  cd "$(dirname "$1")"
-  LINK=$(readlink "$(basename "$1")")
-  while [ "$LINK" ]; do
-    cd "$(dirname "$LINK")"
-    LINK=$(readlink "$(basename "$1")")
-  done
-  REALPATH="$PWD/$(basename "$1")"
-  cd "$OURPWD"
-  echo "$REALPATH"
+my $usage = "$0 -- summarize multiple identical hints to one with mult=n\n"
+	 	. "\n"
+		. "Usage: $0 <in.hints >joined.hints\n"
+		. "  PREREQUISITE: input GFF file must be sorted so that hints that should be summarized are below each other\n"
+		. "  e.g. do a cat hints.gff | sort -n -k 4,4 | sort -s -n -k 5,5 | sort -s -k 3,3 | sort -s -k 1,1 | join_mult_hints.pl\n"
+		. "  hints are only joined if they are from the same src (e.g. src=E)\n"
+		. "  score in column 6 is adapted to mult= value for usage with GeneMark\n";
+
+my $help = 0;
+GetOptions( 'help!' => \$help );
+if ($help) {
+    print "$usage";
+    exit(0);
 }
 
-timezone() {
-    if [ "$(uname)" == "Darwin" ]; then
-        TZ=$(readlink /etc/localtime | sed 's#/var/db/timezone/zoneinfo/##')
-    else
-        TZ=$(readlink /etc/timezone)
-    fi
-    echo $TZ
+my @lf;
+my ( $lm, $m );
+my %identical; # holds all hints with identical coordinates, locus, strand & frame
+
+while ( <STDIN> ) {
+    my @f = split( /\t/, $_ );
+    my $srcKey;
+    if($f[8] =~ m/so?u?rce?=(\w)/){
+    	$srcKey = $1;
+    }else{
+    	$srcKey = 'no_src';
+    }
+    if ( !(@lf) ) {
+        @lf = @f;
+        my @to_be_pushed = @f;
+    	push(@{$identical{$srcKey}}, \@to_be_pushed);
+    }
+    elsif (
+        !(     ( $f[0] eq $lf[0] )
+            && ( $f[2] eq $lf[2] )
+            && ( $f[3] == $lf[3] )
+            && ( $f[4] == $lf[4] )
+            && ( $f[6] eq $lf[6] )
+            && ( $f[7] eq $lf[7] )
+        )
+        )
+    {
+        summarizeHint(\%identical);
+        undef %identical;
+        @lf = @f;
+        my @to_be_pushed = @f;
+        push(@{$identical{$srcKey}}, \@to_be_pushed);
+    }
+    else {
+    	my @to_be_pushed = @f;
+    	push(@{$identical{$srcKey}}, \@to_be_pushed);
+    }
 }
 
+summarizeHint(\%identical);
 
-# Only allocate tty if one is detected. See - https://stackoverflow.com/questions/911168
-if [[ -t 0 ]]; then IT+=(-i); fi
-if [[ -t 1 ]]; then IT+=(-t); fi
-
-WORKDIR="$(realpath .)"
-MOUNT="type=bind,source=${WORKDIR},target=${WORKDIR}"
-TZ="$(timezone)"
-
-# check all arguments for paths to use as mount points
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-all_mounts=$($SCRIPTPATH/dockerized-parser.py "$WORKDIR" "$@")
-
-exec docker run --rm "${IT[@]}" -e TZ="${TZ}" --workdir "${WORKDIR}" $all_mounts augustus join_mult_hints.pl "$@"
+sub summarizeHint {
+	my $hints = shift;
+	foreach my $src (keys %{$hints}) {
+		my @h = @{${$hints->{$src}}[0]};
+		if(scalar(@{$hints->{$src}}) == 1 ) {
+			print join ("\t", @h);
+		}else{
+			my $mult = 0;
+			for (my $i = 0; $i < scalar (@{$hints->{$src}}); $i++ ) {
+				my @l = @{${$hints->{$src}}[$i]};
+				if($l[8] =~ m/mult=(\d+)/) {
+					$mult += $1;
+				}else{
+					$mult++;
+				}
+			}
+			$h[8] =~ s/gro?u?p=[^;]*;//;
+			$h[8] =~ s/mult=\d+;//;
+			for (my $i = 0; $i < 9; $i++) {
+				if( not($i==5) && not ($i==8) ) {
+					print $h[$i]."\t";
+				}elsif($i==5){
+					print $mult."\t";
+				}elsif($i==8){
+					print "mult=$mult;$h[$i]";
+				}
+			}
+		}
+	}
+}
